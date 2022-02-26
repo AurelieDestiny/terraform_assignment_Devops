513c3203c374dc7b6f73f4b15378d63042dd17de
Destynee Martin <aurlie.martin@gmail.com>
2022-02-25 19:56:09 -0500

    update s3 bucket and json item

--- dynamo.tf
+++ dynamo.tf
@@ -1,24 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_dynamodb_table" "airplanes" {
   name = "airplanes"
-  hash_key = "VIN"
-  attibute {
-    name = "VIN"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "PIN"
+  attribute {
+    name = "PIN"
     type = "S"  
-    }
-}
-resource "aws_dynamodb_table_item" "airplane-items" {
-  table_name = aws_dynamodb_table.airplanes.name
-  hash_key   = aws_dynamodb_table.example.hash_key
-  item =  <<EOF
-
-  {
-   "Manufacturere : {"S": "Airbus"},
-   "Make": {"S": "A380"},
-   "Year": {"N": "2009"},
-   "VIN" : "D56GT527VL"
   }
-EOF
+}
+
+resource "aws_dynamodb_table_item" "airplane-types" {
+  table_name = aws_dynamodb_table.airplanes.name
+  hash_key   = aws_dynamodb_table.airplanes.hash_key
+  
+  item = jsonencode({
+   "PIN" : {"S":"D56GT527VL"},
+   "Manufacturer" : {"S": "Airbus"},
+   "Year": {"N": "2009"},
+   "Make": {"S": "A380"},
+   })
 }
--- s3.tf
+++ s3.tf
@@ -1,15 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_s3_bucket" "Devbucket" {
+resource "aws_s3_bucket" "devzbucketz" {
   bucket = "dev-20220225"
   tags = {
     Descriptioin = "Dev projects"  }
 }
 
-resource "aws_s3_bucket_object" "aws-txt" {
-  bucket = "aws_s3_bucket.Devbucket.id"
+resource "aws_s3_object" "awsdocs" {
+  source = "D:/TERRAFORM/Terraform aws_s3/aws.txt"
+  bucket = "dev-20220225"
   key    = "aws.txt"
- 
-} 
+  
+}
+
